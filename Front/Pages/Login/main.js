b40f256a0a99fb0291ebd9428ecec924c7d4224a
Matija Savic <matijasavic3@gmail.com>
2023-12-27 22:59:15 +0100

    Dodat login, register, dashboard

---
+++ Front/Pages/Login/main.js
@@ -0,0 +1,43 @@
+import { serverUrl } from "../../config.js";
+
+
+export class LoginPage {
+    constructor() {
+        this.usernameInput = document.body.querySelector(".username-input");
+        this.passwordInput = document.body.querySelector(".password-input");
+        this.loginButton = document.body.querySelector(".login-button");
+        this.registerButton = document.body.querySelector(".register-button");
+
+        this.user = {username: "", password: ""};
+
+        this.usernameInput.addEventListener("sl-input", ev => this.handleInputChange(ev));
+        this.passwordInput.addEventListener("sl-input", ev => this.handleInputChange(ev));
+        this.loginButton.addEventListener("click", async () => await this.handleLoginClick());
+        this.registerButton.addEventListener("click", () => window.location = "../Register/index.html");
+    }
+
+    handleInputChange(ev) {
+        this.user[ev.target.name] = ev.target.value;
+    }
+
+    async handleLoginClick() {
+        const userLoginRequest = await fetch (serverUrl + "/User/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(this.user)
+        });
+
+        if (!userLoginRequest.ok) {
+            //mozda treba izmena kasnije ovde
+            console.log("ERROR: Login..");
+            return;
+        }
+        
+        localStorage.setItem("id", await userLoginRequest.json());
+        window.location = "../Dashboard/index.html";
+    }
+}
+
+const loginPage = new LoginPage();
---
+++ Front/config.js
@@ -0,0 +1,4 @@
+
+
+export const baseUrl = "http://localhost:5500";
+export const serverUrl = "http://localhost:5062";

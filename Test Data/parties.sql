fdc4344c86d8b13dbf0fbe9463d93f8017c7fede
Matija Savic <matijasavic3@gmail.com>
2024-01-27 14:35:38 +0100

    Izmenjeni test podaci

---
+++ Test Data/parties.sql
@@ -0,0 +1,19 @@
+INSERT INTO "Parties"("Id", "Name", "City", "Address", "Image", "CreatorId")
+VALUES
+(1, 'LepaZurka1', 'LepGrad1', 'LepaAdresa1', 'LepaSlika1', 1),
+(2, 'LepaZurka2', 'LepGrad2', 'LepaAdresa2', 'LepaSlika2', 1),
+(3, 'LepaZurka3', 'LepGrad3', 'LepaAdresa3', 'LepaSlika3', 1),
+(4, 'FinaZurka1', 'FinGrad1', 'FinaAdresa1', 'FinaSlika1', 2),
+(5, 'FinaZurka2', 'FinGrad2', 'FinaAdresa2', 'FinaSlika2', 2),
+(6, 'FinaZurka3', 'FinGrad3', 'FinaAdresa3', 'FinaSlika3', 2),
+(7, 'OkejZurka1', 'OkejGrad1', 'OkejAdresa1', 'OkejSlika1', 7),
+(8, 'LelZurka1', 'LelGrad1', 'LelAdresa1', 'LelSlika1', 8),
+(9, 'LegoZurka1', 'LegoGrad1', 'LegoAdresa1', 'LegoSlika1', 10),
+(10, 'TaskEdit', 'TaskEdit', 'TaskEdit', 'TaskEdit', 11),
+(11, 'PartyToCancel', 'Nis', 'Adresa', 'Slika', 16),
+(12, 'PartyToEdit', 'Nis', 'Adresa', 'Slika', 17),
+(13, 'PartyToAttend', 'Nis', 'Adresa', 'Slika', 15),
+(14, 'PartyToUnattend', 'Nis', 'Adresa', 'Slika', 15),
+(15, 'TaskToCreate', 'Nis', 'Adresa', 'Slika', 20),
+(16, 'TaskToEdit', 'Nis', 'Adresa', 'Slika', 21),
+(17, 'TaskToRemove', 'Nis', 'Adresa', 'Slika', 22);
--- Test Data/tasks.sql
+++ Test Data/tasks.sql
@@ -2,4 +2,6 @@ INSERT INTO "Tasks"("Id", "Name", "Description", "UserId", "PartyId")
 VALUES
 (1, 'VaziTask1', 'Desc', 8, 8),
 (2, 'TaskEdit', 'TaskEdit', 11, 10),
-(3, 'TaskDelete', 'TaskDelete', 11, 10);
+(3, 'TaskDelete', 'TaskDelete', 11, 10),
+(4, 'TaskEdit', 'TaskEdit', 21, 16),
+(5, 'TaskRemove', 'TaskRemove', 22, 17);
